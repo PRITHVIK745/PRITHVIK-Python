2ae97b885c78d3c2b60ec0de2b3e90604ededf5a
PRITHVIK745 <prithvik.s.gowda745@gmail.com>
2026-02-15 21:48:41 +0530

    Add files via upload

---
+++ ConnectX Tasks_files/task11.py
@@ -0,0 +1,16 @@
+class Book:
+    def read_page(self, page):
+        return f"Reading page {page}"
+
+class BatteryPowered:
+    def battery_status(self, level):
+        return f"Battery at {level}%"
+
+class EBookReader(Book, BatteryPowered):
+    pass
+
+e1 = EBookReader()
+print( e1.read_page(10))
+print( e1.battery_status("90"))
+print( e1.read_page(0))
+print( e1.battery_status("0"))
